09b23c36791da371ce8c1ad354827ab8365563e5
Jonathan Rockway <jrockway@users.noreply.github.com>
2020-02-26 22:47:51 -0500

    some tweaks to make the webhook reliable

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/jrockway/alertmanager-discord
+
+go 1.14
+
+require (
+	github.com/apex/log v1.1.2
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.0
+	github.com/jrockway/opinionated-server v0.0.8
+	github.com/prometheus/alertmanager v0.20.0
+	go.uber.org/zap v1.13.0
+)
--- main.go
+++ main.go
@@ -2,101 +2,121 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
-	"strings"
+	"time"
+
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
+	"github.com/jrockway/opinionated-server/client"
+	"github.com/jrockway/opinionated-server/server"
+	"github.com/prometheus/alertmanager/template"
+	"go.uber.org/zap"
 )
-
-type alertManAlert struct {
-	Annotations struct {
-		Description string `json:"description"`
-		Summary     string `json:"summary"`
-	} `json:"annotations"`
-	EndsAt       string            `json:"endsAt"`
-	GeneratorURL string            `json:"generatorURL"`
-	Labels       map[string]string `json:"labels"`
-	StartsAt     string            `json:"startsAt"`
-	Status       string            `json:"status"`
-}
-
-type alertManOut struct {
-	Alerts            []alertManAlert `json:"alerts"`
-	CommonAnnotations struct {
-		Summary string `json:"summary"`
-	} `json:"commonAnnotations"`
-	CommonLabels struct {
-		Alertname string `json:"alertname"`
-	} `json:"commonLabels"`
-	ExternalURL string `json:"externalURL"`
-	GroupKey    string `json:"groupKey"`
-	GroupLabels struct {
-		Alertname string `json:"alertname"`
-	} `json:"groupLabels"`
-	Receiver string `json:"receiver"`
-	Status   string `json:"status"`
-	Version  string `json:"version"`
-}
 
 type discordOut struct {
 	Content string `json:"content"`
-	Name    string `json:"username"`
+}
+
+type appflags struct {
+	WebhookURL string `long:"webhook.url" env:"DISCORD_WEBHOOK" description:"Your Discord webhook URL." required:"true"`
+}
+
+var cl = &http.Client{
+	Transport: client.WrapRoundTripper(nil),
+}
+
+func sendOneAlert(ctx context.Context, url string, alerts *template.Data, alert template.Alert) error {
+	discordMsg := discordOut{}
+	alertJson, err := json.Marshal(alert)
+	if err != nil {
+		return fmt.Errorf("formatting alert: %w", err)
+	}
+	discordMsg.Content = fmt.Sprintf("```%s```", alertJson)
+	body, err := json.Marshal(discordMsg)
+	if err != nil {
+		return fmt.Errorf("marshaling discord message: %w", err)
+	}
+	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewReader(body))
+	if err != nil {
+		return fmt.Errorf("creating http request: %w", err)
+	}
+	req.Header.Add("content-type", "application/json")
+	res, err := cl.Do(req)
+	if err != nil {
+		return fmt.Errorf("executing http request: %w", err)
+	}
+	if res.StatusCode < http.StatusOK || res.StatusCode >= http.StatusBadRequest {
+		return fmt.Errorf("non-200 response from discord: %d %s", res.StatusCode, res.Status)
+	}
+	return nil
+}
+
+func makeWebhookHandler(url string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		logger := ctxzap.Extract(req.Context())
+		b, err := ioutil.ReadAll(req.Body)
+		req.Body.Close()
+		if err != nil {
+			logger.Error("problem reading request body", zap.Error(err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		alerts := new(template.Data)
+		if err := json.Unmarshal(b, &alerts); err != nil {
+			logger.Error("problem unmarshalling alerts", zap.Error(err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if alerts.Alerts == nil || len(alerts.Alerts) < 1 {
+			logger.Error("no alerts provided", zap.ByteString("body", b))
+			http.Error(w, "no alerts provided", http.StatusBadRequest)
+			return
+		}
+
+		var errored bool
+		for _, alert := range alerts.Alerts {
+			if err := sendOneAlert(req.Context(), url, alerts, alert); err != nil {
+				logger.Error("problem sending request", zap.Error(err))
+				errored = true
+			}
+		}
+		if errored {
+			http.Error(w, "problem sending alert; see the logs", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	})
 }
 
 func main() {
-	webhookUrl := os.Getenv("DISCORD_WEBHOOK")
-	whURL := flag.String("webhook.url", webhookUrl, "")
-	flag.Parse()
+	server.AppName = "alertmanager-discord"
 
-	if webhookUrl == "" && *whURL == "" {
-		fmt.Fprintf(os.Stderr, "error: environment variable DISCORD_WEBHOOK not found\n")
-		os.Exit(1)
-	}
-
-	fmt.Fprintf(os.Stdout, "info: Listening on 0.0.0.0:9094\n")
-	http.ListenAndServe(":9094", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		b, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			panic(err)
+	f := new(appflags)
+	server.AddFlagGroup("alertmanager-discord", f)
+	server.Setup()
+	zap.L().Info("webhook", zap.String("url", f.WebhookURL))
+	server.SetHTTPHandler(makeWebhookHandler(f.WebhookURL))
+	server.SetStartupCallback(func(server.Info) {
+		ctx, c := context.WithTimeout(context.Background(), 5*time.Second)
+		defer c()
+		alertData := &template.Data{
+			Alerts: template.Alerts{
+				template.Alert{
+					Status: "Webhook receiver starting up.",
+				},
+			},
 		}
-
-		amo := alertManOut{}
-		err = json.Unmarshal(b, &amo)
-		if err != nil {
-			panic(err)
+		if err := sendOneAlert(ctx, f.WebhookURL, alertData, alertData.Alerts[0]); err != nil {
+			// We don't kill the program here, because Discord could be down.  But at
+			// least you'll see something in the logs before a real alert if your
+			// webhook is broken.
+			zap.L().Error("problem sending initial alert", zap.Error(err))
 		}
-
-		groupedAlerts := make(map[string][]alertManAlert)
-
-		for _, alert := range amo.Alerts {
-			groupedAlerts[alert.Status] = append(groupedAlerts[alert.Status], alert)
-		}
-
-		for status, alerts := range groupedAlerts {
-			DO := discordOut{
-				Name: status,
-			}
-
-			Content := "```"
-			if amo.CommonAnnotations.Summary != "" {
-				Content = fmt.Sprintf(" === %s === \n```", amo.CommonAnnotations.Summary)
-			}
-
-			for _, alert := range alerts {
-				realname := alert.Labels["instance"]
-				if strings.Contains(realname, "localhost") && alert.Labels["exported_instance"] != "" {
-					realname = alert.Labels["exported_instance"]
-				}
-				Content += fmt.Sprintf("[%s]: %s on %s\n%s\n\n", strings.ToUpper(status), alert.Labels["alertname"], realname, alert.Annotations.Description)
-			}
-
-			DO.Content = Content + "```"
-
-			DOD, _ := json.Marshal(DO)
-			http.Post(*whURL, "application/json", bytes.NewReader(DOD))
-		}
-	}))
+	})
+	server.ListenAndServe()
 }
